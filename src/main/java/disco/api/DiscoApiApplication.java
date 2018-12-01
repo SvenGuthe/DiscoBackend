90b72b8d25e7394bf15675cf8b16c3dc71e2a8f3
Sven Guthe <sven.guthe@intenta.de>
2018-12-01 11:36:12 +0100

    Initial Commit

---
+++ src/main/java/disco/api/DiscoApiApplication.java
@@ -0,0 +1,12 @@
+package disco.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoApiApplication.class, args);
+    }
+}
